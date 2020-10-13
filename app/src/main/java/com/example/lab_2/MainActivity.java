a1c05423a4abcafe5cb32f1cbd3af5621f8fc5fe
Eagle-0wl <71759598+Eagle-0wl@users.noreply.github.com>
2020-10-13 18:52:50 +0300

    Added: Word and symbol counters Check user preference what to count(words/symbols) Display Error message when input field is emty.
    Added(2):
    Add seperata class for calculator and move code to it
    Add graphical elements text to strings.xml

--- app/src/main/java/com/example/lab_2/MainActivity.java
+++ app/src/main/java/com/example/lab_2/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private View txtInputBox;
     private TextView tvMain;
-    String text;
+    String spinnerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +34,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-    text = parent.getItemAtPosition(position).toString();
-    Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+    spinnerText = parent.getItemAtPosition(position).toString();
+    Toast.makeText(parent.getContext(),spinnerText, Toast.LENGTH_SHORT).show();
     }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
@@ -44,37 +44,16 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void btnCounter(View view) {
         EditText txtInputBox=findViewById(R.id.txtInputBox);
         String userInput=txtInputBox.getText().toString();
-        int userInputLength=userInput.length();
         Context context = getApplicationContext();
-        if(userInputLength>0) {
-            if (text.equals("Words")) {
-                int wordCount = 0;
-                char[] userInputCharacters = new char[userInputLength];
-                for (int i = 0; i < userInputLength; i++) {
-                    userInputCharacters[i] = userInput.charAt(i);
-                }
-                int i = 0;
-                while (userInputLength - 1 > i) {
-                    if (userInputCharacters[i] == ',' && userInputCharacters[i + 1] != ' ') {
-                        wordCount++;
-                    } else if (userInputCharacters[i] == ',' && userInputCharacters[i + 1] == ' ') {
-                        wordCount++;
-                        i++;
-                    } else if (userInputCharacters[i] == '.' && userInputCharacters[i + 1] != ' ') {
-                        wordCount++;
-                    } else if (userInputCharacters[i] == '.' && userInputCharacters[i + 1] == ' ') {
-                        wordCount++;
-                        i++;
-                    } else if (userInputCharacters[i] == ' ') {
-                        wordCount++;
-                    }
-                    i++;
-                }
-                if (userInputCharacters[userInputLength - 1] != ',' || userInputCharacters[userInputLength - 1] != '.' || userInputCharacters[userInputLength - 1] != ' ')
-                    wordCount++;
-                this.tvMain.setText(Integer.toString(wordCount));
-            } else if (text.equals("Symbols")) {
-                this.tvMain.setText(Integer.toString(userInput.length()));
+        if(TextCounter.countChars(userInput)>0)
+        {
+            if (spinnerText.equals("Words"))
+            {
+                this.tvMain.setText(Integer.toString(TextCounter.countWords(userInput)));
+            }
+            else if (spinnerText.equals("Symbols"))
+            {
+                this.tvMain.setText(Integer.toString(TextCounter.countChars(userInput)));
             }
         }
         else  Toast.makeText(context, "The field is emty", Toast.LENGTH_SHORT).show();
